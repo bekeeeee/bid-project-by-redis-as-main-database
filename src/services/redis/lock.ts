f63c46ed875023a8f31d6dfa4f2c39992b1b22db
bekeeeee <m.ragab9521@gmail.com>
2023-04-20 21:29:46 +0200

    Add concurrency changes

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,35 +1,61 @@
 import { randomBytes } from 'crypto';
 import { client } from './client';
-export const withLock = async (key: string, cb: () => any) => {
+export const withLock = async (key: string, cb: (redisClient: Client, signal: any) => any) => {
 	// Initialize a few variables to control retry behavior
 
 	const retryDelaysMs = 100;
 	let retries = 20;
+	let timeoutMs = 2000;
 
 	// Generate a random value to store at the lock key
 	const token = randomBytes(6).toString('hex');
 	// create the lock key
 
 	const lockKey = `lock:${key}`;
+	// await client.
 
 	// set up a while loop to implement retry behaviour
 
 	while (retries-- > 0) {
 		// try to do a SET NX operation
 		const state = await client.set(lockKey, token, {
-			NX: true
+			NX: true,
+			PX: 2000
 		});
-		if (state) {
-			client.del(lockKey);
-			const result = await cb();
-			return result;
+		if (!state) {
+			await pause(retryDelaysMs);
+			continue;
 		}
-
-		await pause(retryDelaysMs);
+		try {
+			const signal = {
+				expired: false
+			};
+			setTimeout(() => {
+				signal.expired = true;
+			}, timeoutMs);
+			const proxiedClient = buildClientProxy(timeoutMs);
+			const result = await cb(proxiedClient, signal);
+			return result;
+		} finally {
+			client.unlock(lockKey, token);
+		}
 	}
 };
 
-const buildClientProxy = () => {};
+type Client = typeof client;
+
+const buildClientProxy = (timeoutMs: number) => {
+	const startTime = Date.now();
+
+	const handler = {
+		get(target: Client, prop: keyof Client) {
+			if (Date.now() >= startTime + timeoutMs) throw new Error('Lock has expired');
+			const value = target[prop];
+			return typeof value === 'function' ? value.bind(target) : value;
+		}
+	};
+	return new Proxy(client, handler) as Client;
+};
 
 const pause = (duration: number) => {
 	return new Promise((resolve) => {
--- worker/client.ts
+++ worker/client.ts
@@ -6,30 +6,7 @@ const client = createClient({
 		port: parseInt(process.env.REDIS_PORT!)
 	},
 	// password: process.env.REDIS_PW
-	legacyMode: true,
-	scripts: {
-		incrementView: defineScript({
-			NUMBER_OF_KEYS: 3,
-			SCRIPT: `
-				local itemsViewsKey = KEYS[1]
-				local itemsKey = KEYS[2]
-				local itemsByViewsKey = KEYS[3]
-				local itemId = ARGV[1]
-				local userId = ARGV[2]
-
-				local inserted = redis.call('PFADD', itemsViewsKey, userId)
-
-				if inserted == 1 then
-					redis.call('HINCRBY', itemsKey, 'views', 1)
-					redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
-				end
-			`,
-			transformArguments(itemId: string, userId: string) {
-				return [itemsViewsKey(itemId), itemsKey(itemId), itemsByViewsKey(), itemId, userId];
-			},
-			transformReply() {}
-		})
-	}
+	legacyMode: true
 });
 
 client.on('error', (err) => console.log(err));
